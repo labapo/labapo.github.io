73b914c0d71eaa3f5064b64ac19e07200ecea174
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2023-01-31 22:45:29 -0700

    added resume

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
 
 const Intro = () => {
   return (
     <div>
-    <h1>Intro</h1>
-    <p>Here is a paragraph</p>
+    <Typography variant="h1">
+      <h1>About Me</h1>
+    </Typography>
+    <Typography variant="body1">
+        <p>
+          Thank you for visiting my page! I am currently attending a Software
+          Engineering BootCamp at Per Scholas with an anticipated graduation
+          date of February 2023. 
+        </p>
+        <br />
+        <p>
+          My professional experience includes:
+          <br />
+          <ul>
+            <li>Teaching: College and Elementary Education</li>
+            <li>Marketing</li>
+            <li>Product Support and Integration</li>
+            <li>Education Technology</li>
+          </ul>
+        </p>
+      <div>
+        <p>
+          I am seeking a software engineering position in Education Technology.
+          Throughout my career, I have been able to view this industry from
+          various lenses that allow me to understand how users engage with
+          technology and what use cases are important. The bulk of my experience
+          includes supporting schools from a central office level within a
+          charter school network. Additionally, I have experience in software
+          implementation with various districts across the country. Lastly, I've
+          also been on the ground level working with students and faculty,
+          making me well-versed in multiple areas of Education Technology.  ​
+          I'm highly passionate about education equity and helping with
+          improving student outcomes. I would love to continue using my
+          knowledge to make quality-of-life changes for the education sector as
+          a Software Engineer. 
+        </p>
+      </div>
+    </Typography>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
-const Resume = () => {
+export default function Resume() {
   return (
-    <h1>
-      Here is some content
-    </h1>
-  )
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Professional Summary</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Education Technology professional with a background in teaching,
+            product support and integration, and project management. Seeking an
+            opportunity as a Software Engineer to support the education sector.
+            Strengths include troubleshooting, cross-team collaboration, and
+            mediating between stakeholders and the development team. Highly
+            passionate about education equity and helping with positive student
+            outcomes. MERN Stack Certification is expected in February 2023.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Education</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <ul>
+              <li>
+                {" "}
+                PerScholas│Software Engineering Student│Denver, CO Completion in
+                Feb. 2023{" "}
+              </li>
+              <li>
+                {" "}
+                Touro College│M.A General and Special Education│New York, NY
+                2015
+              </li>
+              <li>
+                {" "}
+                University of North Florida │ M.A. Applied Ethics and Philosophy
+                │ Jacksonville, FL 2012
+              </li>
+              <li>
+                {" "}
+                University of Central Florida │Philosophy │Orlando, FL 2010
+              </li>
+            </ul>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Experience</Typography>
+        </AccordionSummary>
+        <AccordionDetails variant="body1">
+          EDUCATION TECHNOLOGY INTEGRATIONS SPECIALIST March 2021 – June 2022
+          American Reading Company Managed projects related to the development
+          of new ed-tech integrations, maintenance, and product upgrades
+          Assisted customers to resolve issues with integrations between digital
+          products and their ed-tech platforms such as Clever and ClassLink
+          Consulted district technologists during pre-implementation calls
+          Created interactive dashboards to identify the needs of customers and
+          created training articles to address knowledge gaps Maintained vendor
+          relations
+          <br />
+          EDUCATION TECHNOLOGY COORDINATOR Oct. 2018 – Feb. 2021 Denver School
+          of Science and Technology Partnered with schools to ensure that
+          student data—including schedules, grades, and transcripts—meets a high
+          bar of excellence for data integrity and efficient systematization
+          Managed Zendesk tickets queue and provide in-school and remote support
+          to teachers, faculty, and students Collaborated with academic and
+          software team, school leadership, and central office leadership to
+          implement an assessment platform, Edulastic, to 15 schools in 2 school
+          districts
+          <br />
+          MARKETING ASSISTANT June 2017 – Oct. 2018 Macmillan Audio Created and
+          edited content for Macmillan Audio blog using Wordpress, increasing
+          web traffic by 22% Developed system of tracking marketing initiatives
+          and maintaining budgets for over 450 titles Created landing pages,
+          scripts for podcasts, and copy for email marketing campaigns
+          <br />
+          SCIENCE LEAD TEACHER Apr. 2014 – Dec. 2016 Success Academy Charter
+          School Analyzed and tracked data to push 100% of 4th-grade students to
+          pass the NYS Science Exam with the highest possible score of a 4
+          Collaborated across grade teams about interventions and strategies to
+          support learning by analyzing data to inform goals and objectives
+          Planned, organized, and altered lesson plans for multiple grades,
+          learning objectives, and differentiated instruction
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
 }
-
-export default Resume
