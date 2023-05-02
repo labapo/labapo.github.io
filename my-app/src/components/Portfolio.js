cf96cae6d683091641144dba1b5d911c9064a6ac
Lorivie Abapo <labapo@gmail.com>
2023-05-02 12:55:50 -0600

    updated portfolio tab and buttons

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -2,8 +2,8 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Jeopardy from "../images/Jeopardy.png";
 import memories from "../images/memories.png";
-import Placeholder from "../images/Placeholder.png";
 import Button from "react-bootstrap/Button";
+import Dashboard from "../images/Dashboard.png"
 
 function Portfolio() {
   return (
@@ -14,6 +14,9 @@ function Portfolio() {
         </a>
         <Carousel.Caption>
           <h4 className="caption-text">Jeopardy</h4>
+          <Button className="button-portfolio"variant="primary" href="https://jeopardy-6nfr.onrender.com">
+          Jeopardy Game
+        </Button>
           <Button variant="primary" href="https://github.com/labapo/jeopardy-game">
           GitHub
         </Button>
@@ -25,21 +28,23 @@ function Portfolio() {
         </a>
         <Carousel.Caption>
           <h4 className="caption-text">Memories App</h4>
+          <Button className="button-portfolio"variant="primary" href="https://memories-app-nia4.onrender.com">
+          Memories Site
+        </Button>
           <Button variant="primary" href="https://github.com/labapo/memories">
           GitHub
         </Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={Placeholder} alt="Third slide" />
+        <img className="d-block w-100" src={Dashboard} alt="Third slide" />
 
         <Carousel.Caption>
           <h4 className="caption-text">Tableau Dashboard</h4>
-          <p>
-          <Button className="primary" variant="primary" href="https://github.com/labapo/mern-app-final-project">
-          GitHub
+          
+          <Button variant="primary" href="https://public.tableau.com/app/profile/lorivie.abapo/viz/Dashboard_HMEA/TimeKeepCoExpansion">
+          Tableau Public
         </Button>
-        </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
